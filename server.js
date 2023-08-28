8c654b7cbb16781322954484d00e450e16c8c77d
rimi <a.fatimaarimi@gmail.com>
2023-08-28 17:29:30 -0400

    HW Complete

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-// ! i forgot to npm i express and it threw an error
 const express = require("express");
 const app = express();
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("aha!")
+  res.send(
+    `99 bottles of beer on the wall <br>
+    <a href='/99'>Take one down, pass it around</a>`
+  );
 });
 
-app.listen(PORT, (req, res) => {
-    console.log(`Server is now running on localhost:${PORT}`);
+app.get("/:numsOfBottle", (req, res) => {
+  const numsOfBottle = parseInt(req.params.numsOfBottle); // Parse the parameter as an integer
+  const update_quantity = numsOfBottle - 1;
+
+  if (numsOfBottle > 0) {
+    res.send(
+      `${numsOfBottle} bottles of beer on the wall<br>Take one down, pass it around <br><a href='/${update_quantity}'>${update_quantity} bottles of beer on the wall</a>`
+    );
+  } else if (numsOfBottle === 0) {
+    res.send(`Uh-oh, done already?`);
+  } else {
+    res.send(`Invalid input`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is now running on localhost:${PORT}`);
 });
